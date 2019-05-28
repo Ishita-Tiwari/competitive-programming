78a39f1f1ac108abda8385e7ec52044593381234
Ishita Tiwari <ishitatiwari23@gmail.com>
2019-05-28 10:27:34 +0530

    Add files via upload
    Link to problem - https://www.codechef.com/problems/GRIDTOUR

---
+++ Codechef/GRIDTOUR.py
@@ -0,0 +1,17 @@
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b%a , a)
+
+
+
+t = int(input())
+while(t > 0):
+    n, m, k = [int(x) for x in input().split()]
+    valN = gcd(n, k)
+    valM = gcd(m, k)
+    if valN == 1 and valM == 1:
+        print(n * m)
+    else:
+        print(-1)
+    t -= 1
