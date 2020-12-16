681355588a48c29ee7969cfbd68e9365e4b6f05e
Ishita Tiwari <ishitatiwari23@gmail.com>
2020-12-16 23:24:41 +0530

    Add files via upload
    Link to the problem - https://www.codechef.com/problems/MATBREAK

---
+++ Codechef/MATBREAK.py
@@ -0,0 +1,16 @@
+t = int(input())
+mod = 10**9 + 7
+for T in range(t):
+    n, a = [int(x) for x in input().split()]
+    curr = a
+    multiplier = 1
+    rest = a
+    ans = 0
+    
+    for i in range(1, n + 1):
+        curr = pow(curr, multiplier, mod)
+        rest = (curr % mod * rest % mod) % mod
+        ans = (ans % mod + curr) % mod
+        curr = rest
+        multiplier += 2
+    print(ans)
