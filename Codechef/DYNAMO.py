d4425c6f94f3a4f59606b64064436630d84c235a
Ishita Tiwari <ishitatiwari23@gmail.com>
2021-01-16 00:39:02 +0530

    Add files via upload
    Link to the problem - https://www.codechef.com/JAN20B/problems/DYNAMO

---
+++ Codechef/DYNAMO.py
@@ -0,0 +1,11 @@
+t = int(input())
+for T in range(t):
+    n = int(input())
+    n1 = int(input())
+    total = pow(10, n)
+    print(total * 2 + n1)
+    n3 = int(input())
+    print(total - n3)
+    n5 = int(input())
+    print(total - n5)
+    v = int(input()) 
